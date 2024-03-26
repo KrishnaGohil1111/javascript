37f34b8cf05e439159e36c38a4c97bd654613c1d
KrishnaGohil1111 <137959557+KrishnaGohil1111@users.noreply.github.com>
2024-03-26 14:44:41 +0000

    conversion of datatype

---
+++ 01_BASICS/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+console.log(typeof score);
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+
+// "33" = 33
+// "33abc" = NaN
+// True  => 1 or false => 0
+
+let isloginIN = 1
+let booleanisloginIN = Boolean(isloginIN)
+console.log(booleanisloginIN);
+
+// 1 => true or 0=> false
+// "" => false
+// "hitesh => true"
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
