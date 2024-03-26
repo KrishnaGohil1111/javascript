ef863205e67ba3df29f3cc40548b9fdc14334c3e
KrishnaGohil1111 <137959557+KrishnaGohil1111@users.noreply.github.com>
2024-03-26 08:02:13 +0000

    datatypes

--- 01_BASICS/02_datatypes.js
+++ 01_BASICS/02_datatypes.js
@@ -2,3 +2,20 @@
 
 
 // alert(3+3) we are using nodejs, not browser
+let name = "krishna"
+let age = 18
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof "krishna");
+console.log(typeof undefined);
+console.log(typeof null);
+
