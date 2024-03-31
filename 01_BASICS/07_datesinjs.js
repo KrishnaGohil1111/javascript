18e7a21d9d7bca0ed1bcd6b770cd2ab05716ad05
KrishnaGohil1111 <137959557+KrishnaGohil1111@users.noreply.github.com>
2024-03-31 14:25:03 +0000

    container config done

--- 01_BASICS/07_datesinjs.js
+++ 01_BASICS/07_datesinjs.js
@@ -3,3 +3,15 @@ console.log(mydate.toString());
 console.log(mydate.toDateString());
 console.log(mydate.toLocaleString());
 console.log(typeof mydate);
+
+// let mycreateddate = new Date(20,3,23)
+// let mycreateddate = new Date(20,0,23,5,3)
+// let mycreateddate = new Date("2023-1-14")
+let mycreateddate = new Date("01-14-2023")
+console.log(mycreateddate.toDateString());
+
+let mytimestamp = Date.now()
+console.log(mytimestamp);
+console.log(mycreateddate.getTime());
+
+
