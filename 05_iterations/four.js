e399c422fada31475471c9f115f3b7634b29b46c
KrishnaGohil1111 <137959557+KrishnaGohil1111@users.noreply.github.com>
2024-04-06 09:05:54 +0000

    done

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,20 +1,19 @@
-// for of
-
-// ["", "", ""]
-// [{}, {}, {}]
-
-const arr = [1, 2, 3, 4, 5]
-
-for (const num of arr) {
-    //console.log(num);
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
 }
 
-const greetings = "Hello world!"
-for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
-// Maps
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
 
 const map = new Map()
 map.set('IN', "India")
@@ -22,19 +21,6 @@ map.set('USA', "United States of America")
 map.set('Fr', "France")
 map.set('IN', "India")
 
-
-// console.log(map);
-
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+for (const key in map) {
+    console.log(key);
 }
-
-const myObject = {
-    game1: 'NFS',
-    game2: 'Spiderman'
-}
-
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
-// }
